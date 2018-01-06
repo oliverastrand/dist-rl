97a8ac8f918f937a43d3c736ab15c17cf4a2da55
oliverastrand <oliver.astrand@gmail.com>
2018-01-06 13:47:21 +0100

    Changed env string

--- src/pong_agent.py
+++ src/pong_agent.py
@@ -14,7 +14,7 @@ class Agent:
     def __init__(self, n_win_ticks=195, gamma=1.0, epsilon=1.0, epsilon_min=0.01,
                  epsilon_log_decay=0.995, alpha=0.01, alpha_decay=0.01, batch_size=64, monitor=False):
         self.memory = deque(maxlen=100000)
-        self.env = gym.make('CartPole-v0')
+        self.env = gym.make('Pong-v0')
         if monitor: self.env = gym.wrappers.Monitor(self.env, '../data/cartpole-1', force=True)
         self.gamma = gamma
         self.epsilon = epsilon
