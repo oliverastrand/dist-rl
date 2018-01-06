311082c331872f9050d8e93358cace601e70499c
OscarXingLuo <33905072+OscarXingLuo@users.noreply.github.com>
2018-01-06 23:16:47 +0100

    Update pong_learner.py with Conv-Nets
    3 layers of conv-nets and 1 fully-connected layer

--- src/pong_learner.py
+++ src/pong_learner.py
@@ -6,6 +6,21 @@ class Learner:
     def __init__(self, observation_shape, nr_actions, alpha=0.01):
 
         self.alpha = alpha
+        self.conv1_size = 10
+        self.conv1_filters = 32
+        self.conv1_stride = [1, 5, 5, 1]
+
+        self.conv2_size = 4
+        self.conv2_filters = 64
+        self.conv2_stride = [1, 2, 2, 1]
+
+        self.conv3_size = 3
+        self.conv3_filters = 64
+        self.conv3_stride = [1, 1, 1, 1]
+
+        self.FCN_size = 256
+
+        self.pkeep = 0.75
         self.nr_hidden_1 = 24
         self.nr_hidden_2 = 48
 
@@ -54,20 +69,40 @@ class Learner:
             initial = tf.constant(0.1, shape=shape)
             return tf.Variable(initial, name=name)
 
-        bias1 = bias_variable((self.nr_hidden_1,), "Bias1")
-        wei1 = weight_variable(self.observation_shape + (self.nr_hidden_1,), "Weights1")
-        bias2 = bias_variable((self.nr_hidden_2,), "Bias2")
-        wei2 = weight_variable((self.nr_hidden_1, self.nr_hidden_2), "Weights2")
-        bias3 = bias_variable((self.nr_actions,), "Bias3")
-        wei3 = weight_variable((self.nr_hidden_2, self.nr_actions), "Weights3")
+        def conv2d(x, W, stride):
+            return tf.nn.conv2d(x, W, strides=stride, padding='SAME')
 
-        weight_list = [wei1, bias1, wei2, bias2, wei3, bias3]
+        #CONVOLUTIONAL LAYERS
+        w_conv1 = weight_variable((self.conv1_size, self.conv1_size, 3,self.conv1_filters), "Weights1")
+        b_conv1 = bias_variable([self.conv1_filters],"Bias1")
 
-        hidden_1 = tf.nn.tanh(tf.matmul(input_values, wei1) + bias1)
-        hidden_2 = tf.nn.tanh(tf.matmul(hidden_1, wei2) + bias2)
-        out_layer = tf.matmul(hidden_2, wei3) + bias3
+        w_conv2 = weight_variable((self.conv2_size, self.conv2_size, self.conv1_filters,self.conv2_filters), "Weights2")
+        b_conv2 = bias_variable([self.conv2_filters],"Bias2")
 
-        return out_layer, weight_list
+        w_conv3 = weight_variable((self.conv3_size, self.conv3_size, self.conv2_filters,self.conv3_filters), "Weights3")
+        b_conv3 = bias_variable([self.conv3_filters],"Bias3")
+
+        h_conv1 = tf.nn.relu(conv2d(input_values, w_conv1, self.conv1_stride) + b_conv1  )
+        h_conv2 = tf.nn.relu(conv2d(h_conv1, w_conv2, self.conv2_stride) + b_conv2  )
+        h_conv3 = tf.nn.relu(conv2d(h_conv2, w_conv3, self.conv3_stride) + b_conv3  )
+
+        #FULLY CONNECTED LAYER
+        flat = tf.reshape(h_conv3, [-1, 21 * 16 * self.conv3_filters])
+        w_full = weight_variable([21 * 16 * self.conv3_filters, self.FCN_size],'Weights4')
+        b_full = bias_variable([self.FCN_size],"Bias4")
+
+        dense = tf.matmul(flat, w_full)
+        dense = tf.nn.relu(dense+b_full)
+        densed = tf.nn.dropout(dense,self.pkeep)
+
+        #FINAL OUTPUT LAYER
+        w_final = weight_variable([self.FCN_size, self.nr_actions],"Weights5")
+        b_final = bias_variable([self.nr_actions],"Bias5")
+
+        Ylogits = tf.matmul(densed,w_final) + b_final
+        weight_list = [w_conv1, b_conv1, w_conv2, b_conv2, w_conv3, b_conv3, w_full, b_full,w_final, b_final]
+
+        return Ylogits, weight_list
 
     def get_weights(self):
         return self.weight_list
