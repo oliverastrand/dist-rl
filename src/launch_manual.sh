259dfe10936df2b9666df92320768bc90c30ce95
EC2 Default User <ec2-user@ip-172-31-0-178.eu-west-1.compute.internal>
2018-01-07 21:41:02 +0000

    Add laucnh manual

---
+++ src/launch_manual.sh
@@ -0,0 +1,9 @@
+python main_dist_pong.py ps 0 &
+python main_dist_pong.py worker 0 &
+python main_dist_pong.py worker 1 &
+python main_dist_pong.py worker 2 &
+python main_dist_pong.py worker 3 &
+python main_dist_pong.py worker 4 &
+python main_dist_pong.py worker 5 &
+python main_dist_pong.py worker 6 &
+python main_dist_pong.py worker 7 &
