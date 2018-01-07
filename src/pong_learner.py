3a168f174246e334da6bb44c454dfcd74e288976
OscarXingLuo <33905072+OscarXingLuo@users.noreply.github.com>
2018-01-07 16:23:48 +0100

    Made a fully functional conversion-net+pooling
    3 layers of conv3d, pooling on all three.

--- src/pong_learner.py
+++ src/pong_learner.py
@@ -8,21 +8,21 @@ class Learner:
         self.alpha = alpha
         self.conv1_size = 10
         self.conv1_filters = 32
-        self.conv1_stride = [1, 5, 5, 1]
+        self.conv1_stride = [1, 4, 4, 4, 1]
 
         self.conv2_size = 4
         self.conv2_filters = 64
-        self.conv2_stride = [1, 2, 2, 1]
+        self.conv2_stride = [1, 4, 4, 4, 1]
 
         self.conv3_size = 3
         self.conv3_filters = 64
-        self.conv3_stride = [1, 1, 1, 1]
+        self.conv3_stride = [1, 2, 2, 2, 1]
 
         self.FCN_size = 256
 
         self.pkeep = 0.75
-        self.nr_hidden_1 = 24
-        self.nr_hidden_2 = 48
+        self.kernel_size = [1, 1, 3, 3, 1]
+        self.pool_strides = [1, 3, 3, 3, 1]
 
         self.observation_shape = observation_shape
         self.nr_actions = nr_actions
@@ -69,26 +69,30 @@ class Learner:
             initial = tf.constant(0.1, shape=shape)
             return tf.Variable(initial, name=name)
 
-        def conv2d(x, W, stride):
-            return tf.nn.conv2d(x, W, strides=stride, padding='SAME')
+        def conv3d(x, W, stride):
+            return tf.nn.conv3d(x, W, strides=stride, padding='SAME')
+
+        def pool(h):
+            return tf.nn.max_pool3d(h,self.kernel_size,self.pool_strides, padding = 'SAME')
+
 
         #CONVOLUTIONAL LAYERS
-        w_conv1 = weight_variable((self.conv1_size, self.conv1_size, 3,self.conv1_filters), "Weights1")
+        w_conv1 = weight_variable((self.conv1_size, self.conv1_size, 3, 4,self.conv1_filters), "Weights1")
         b_conv1 = bias_variable([self.conv1_filters],"Bias1")
 
-        w_conv2 = weight_variable((self.conv2_size, self.conv2_size, self.conv1_filters,self.conv2_filters), "Weights2")
+        w_conv2 = weight_variable((self.conv2_size, self.conv2_size, 3, self.conv1_filters,self.conv2_filters), "Weights2")
         b_conv2 = bias_variable([self.conv2_filters],"Bias2")
 
-        w_conv3 = weight_variable((self.conv3_size, self.conv3_size, self.conv2_filters,self.conv3_filters), "Weights3")
+        w_conv3 = weight_variable((self.conv3_size, self.conv3_size, 3, self.conv2_filters,self.conv3_filters), "Weights3")
         b_conv3 = bias_variable([self.conv3_filters],"Bias3")
 
-        h_conv1 = tf.nn.relu(conv2d(input_values, w_conv1, self.conv1_stride) + b_conv1  )
-        h_conv2 = tf.nn.relu(conv2d(h_conv1, w_conv2, self.conv2_stride) + b_conv2  )
-        h_conv3 = tf.nn.relu(conv2d(h_conv2, w_conv3, self.conv3_stride) + b_conv3  )
+        h_conv1 = pool(tf.nn.relu(conv3d(input_values, w_conv1, self.conv1_stride) + b_conv1  ))
+        h_conv2 = pool(tf.nn.relu(conv3d(h_conv1, w_conv2, self.conv2_stride) + b_conv2  ))
+        h_conv3 = pool(tf.nn.relu(conv3d(h_conv2, w_conv3, self.conv3_stride) + b_conv3  ))
 
         #FULLY CONNECTED LAYER
-        flat = tf.reshape(h_conv3, [-1, 21 * 16 * self.conv3_filters])
-        w_full = weight_variable([21 * 16 * self.conv3_filters, self.FCN_size],'Weights4')
+        flat = tf.reshape(h_conv3, [-1, 64])
+        w_full = weight_variable([64, self.FCN_size],'Weights4')
         b_full = bias_variable([self.FCN_size],"Bias4")
 
         dense = tf.matmul(flat, w_full)
@@ -98,8 +102,8 @@ class Learner:
         #FINAL OUTPUT LAYER
         w_final = weight_variable([self.FCN_size, self.nr_actions],"Weights5")
         b_final = bias_variable([self.nr_actions],"Bias5")
-
         Ylogits = tf.matmul(densed,w_final) + b_final
+
         weight_list = [w_conv1, b_conv1, w_conv2, b_conv2, w_conv3, b_conv3, w_full, b_full,w_final, b_final]
 
         return Ylogits, weight_list
