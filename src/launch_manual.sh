b44dc10fdcdc2def9abc1d3cfd8d0d8e845b3bad
EC2 Default User <ec2-user@ip-172-31-0-178.eu-west-1.compute.internal>
2018-01-08 10:51:06 +0000

    Fix

--- src/launch_manual.sh
+++ src/launch_manual.sh
@@ -1,5 +1,9 @@
 python main_dist_pong.py ps 0 &
-python main_dist_pong.py worker 0 &
-python main_dist_pong.py worker 1 &
-python main_dist_pong.py worker 2 &
-python main_dist_pong.py worker 3 &
+CUDA_VISIBLE_DEVICES=0 python main_dist_pong.py worker 0 &
+CUDA_VISIBLE_DEVICES=1 python main_dist_pong.py worker 1 &
+CUDA_VISIBLE_DEVICES=2 python main_dist_pong.py worker 2 &
+CUDA_VISIBLE_DEVICES=3 python main_dist_pong.py worker 3 &
+CUDA_VISIBLE_DEVICES=4 python main_dist_pong.py worker 4 &
+CUDA_VISIBLE_DEVICES=5 python main_dist_pong.py worker 5 &
+CUDA_VISIBLE_DEVICES=6 python main_dist_pong.py worker 6 &
+CUDA_VISIBLE_DEVICES=7 python main_dist_pong.py worker 7 &
